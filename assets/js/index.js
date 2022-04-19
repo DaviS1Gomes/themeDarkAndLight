e99b42ceacef5385befe3fccd5e793b4e4d19f98
Davi Gomes <daviszgomes@gmail.com>
2022-04-19 20:13:46 -0300

    Bug In Divs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector('.btn');
 const container = document.querySelector('.container');
+const circle = document.querySelector('.circle');
+const circlefull = document.querySelector('.circlefull');
+
 
 btn.onclick = function(){
    this.classList.toggle('active');
    container.classList.toggle('active');
+  }
+  
+circlefull.onclick = function(){
+  this.classList.toggle('active');
+  circle.classList.toggle('active');
 }
